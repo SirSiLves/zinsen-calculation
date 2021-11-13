83c3003b683f5ee7b39ade2cbe3d398911e0cdcc
Yves Ruosch <yves@ruosch.me>
2021-11-14 00:25:39 +0100

    calc update

--- src/main/java/me/ruosch/zinsen/Function.java
+++ src/main/java/me/ruosch/zinsen/Function.java
@@ -31,7 +31,7 @@ public class Function {
                     authLevel = AuthorizationLevel.ANONYMOUS)
                     HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
+//        context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
         final String param1 = request.getQueryParameters().get("period");
